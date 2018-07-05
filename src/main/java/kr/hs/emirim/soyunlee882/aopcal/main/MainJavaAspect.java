33ea87fe58eed29354f4a5f4daac714c6912cadf
soyunlee <soyunlee882@gmail.com>
2018-07-05 17:17:15 +0900

    실행시간 측정하기 4 - 프록시 객체로 시간 측정

--- src/main/java/kr/hs/emirim/soyunlee882/aopcal/main/MainJavaAspect.java
+++ src/main/java/kr/hs/emirim/soyunlee882/aopcal/main/MainJavaAspect.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.soyunlee882.aopcal.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 import kr.hs.emirim.soyunlee882.aopcal.cal.Calculator;
 import kr.hs.emirim.soyunlee882.aopcal.config.JavaConfig;
 
@@ -10,9 +11,9 @@ public class MainJavaAspect {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx =
 				new AnnotationConfigApplicationContext(JavaConfig.class);
-		
-		Calculator impCal = ctx.getBean("impCal", Calculator.class);
-		long fiveFact1 = impCal.factorial(5);
+
+		Calculator impeCal = ctx.getBean("impCal", Calculator.class);
+		long fiveFact1 = impeCal.factorial(5);
 		System.out.println("impCal.factorial(5) = " + fiveFact1);
 		
 		Calculator recCal = ctx.getBean("recCal", Calculator.class);
--- src/main/java/kr/hs/emirim/soyunlee882/aopcal/main/MainProxy.java
+++ src/main/java/kr/hs/emirim/soyunlee882/aopcal/main/MainProxy.java
@@ -7,10 +7,11 @@ import kr.hs.emirim.soyunlee882.aopcal.cal.ExeTimeCalculator;
 public class MainProxy {
 
 	public static void main(String[] args) {
+		int num=20;
 		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
-		System.out.println(ttCal1.factorial(5));
+		System.out.println(ttCal1.factorial(num));
 
 		ExeTimeCalculator ttCal2 = new ExeTimeCalculator(new RecCalculator());
-		System.out.println(ttCal2.factorial(5));
+		System.out.println(ttCal2.factorial(num));
 	}
 }
